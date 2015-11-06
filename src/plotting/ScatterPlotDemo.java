020ffa2e46b59ae3ab74d4bba0269c1720c11e19
Justin Cather <catherjustin@gmail.com>
2015-11-05 21:16:59 -0500

    Stuff for plotting

---
+++ src/plotting/ScatterPlotDemo.java
@@ -0,0 +1,124 @@
+package plotting;
+
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.time.Month;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+/**
+ * A demo scatter plot.
+ *
+ * @author David Gilbert
+ */
+public class ScatterPlotDemo extends ApplicationFrame {
+
+    /**
+     * A demonstration application showing a scatter plot.
+     *
+     * @param title  the frame title.
+     */
+    public ScatterPlotDemo(String title) {
+
+        super(title);
+        XYDataset data = createDataset();
+        JFreeChart chart = ChartFactory.createScatterPlot(
+            "Scatter Plot Demo",
+            "X", "Y", 
+            data, 
+            PlotOrientation.VERTICAL,
+            true, 
+            true, 
+            false
+        );
+        NumberAxis domainAxis = (NumberAxis) chart.getXYPlot().getDomainAxis();
+        domainAxis.setAutoRangeIncludesZero(false);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        chartPanel.setVerticalAxisTrace(true);
+        chartPanel.setHorizontalAxisTrace(true);
+        //chartPanel.setVerticalZoom(true);
+        //chartPanel.setHorizontalZoom(true);
+        setContentPane(chartPanel);
+
+    }
+
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     */
+    public static void main(String[] args) {
+
+        ScatterPlotDemo demo = new ScatterPlotDemo("Scatter Plot Demo");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+
+    }
+    private static XYDataset createDataset(){
+    	TimeSeries s1 = new TimeSeries("L&G European Index Trust");
+        s1.add(new Month(2, 2001), 181.8);
+        s1.add(new Month(3, 2001), 167.3);
+        s1.add(new Month(4, 2001), 153.8);
+        s1.add(new Month(5, 2001), 167.6);
+        s1.add(new Month(6, 2001), 158.8);
+        s1.add(new Month(7, 2001), 148.3);
+        s1.add(new Month(8, 2001), 153.9);
+        s1.add(new Month(9, 2001), 142.7);
+        s1.add(new Month(10, 2001), 123.2);
+        s1.add(new Month(11, 2001), 131.8);
+        s1.add(new Month(12, 2001), 139.6);
+        s1.add(new Month(1, 2002), 142.9);
+        s1.add(new Month(2, 2002), 138.7);
+        s1.add(new Month(3, 2002), 137.3);
+        s1.add(new Month(4, 2002), 143.9);
+        s1.add(new Month(5, 2002), 139.8);
+        s1.add(new Month(6, 2002), 137.0);
+        s1.add(new Month(7, 2002), 132.8);
+
+        TimeSeries s2 = new TimeSeries("L&G UK Index Trust");
+        s2.add(new Month(2, 2001), 129.6);
+        s2.add(new Month(3, 2001), 123.2);
+        s2.add(new Month(4, 2001), 117.2);
+        s2.add(new Month(5, 2001), 124.1);
+        s2.add(new Month(6, 2001), 122.6);
+        s2.add(new Month(7, 2001), 119.2);
+        s2.add(new Month(8, 2001), 116.5);
+        s2.add(new Month(9, 2001), 112.7);
+        s2.add(new Month(10, 2001), 101.5);
+        s2.add(new Month(11, 2001), 106.1);
+        s2.add(new Month(12, 2001), 110.3);
+        s2.add(new Month(1, 2002), 111.7);
+        s2.add(new Month(2, 2002), 111.0);
+        s2.add(new Month(3, 2002), 109.6);
+        s2.add(new Month(4, 2002), 113.2);
+        s2.add(new Month(5, 2002), 111.6);
+        s2.add(new Month(6, 2002), 108.8);
+        s2.add(new Month(7, 2002), 101.6);
+
+        // ******************************************************************
+        //  More than 150 demo applications are included with the JFreeChart
+        //  Developer Guide...for more information, see:
+        //
+        //  >   http://www.object-refinery.com/jfreechart/guide.html
+        //
+        // ******************************************************************
+
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(s1);
+        dataset.addSeries(s2);
+
+        return dataset;
+    }
+}
+
